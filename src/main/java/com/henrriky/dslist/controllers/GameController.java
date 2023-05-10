1a0be662b1744f9913105d9c8fa1fe19f62a06cd
Henrriky <tectutors.123@gmail.com>
2023-05-10 15:00:14 -0300

    GameMinDTO, GameRepository, GameService and GameController

---
+++ src/main/java/com/henrriky/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.henrriky.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.henrriky.dslist.dto.GameMinDTO;
+import com.henrriky.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+	
+}
---
+++ src/main/java/com/henrriky/dslist/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.henrriky.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.henrriky.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+	
+	
+	
+}
